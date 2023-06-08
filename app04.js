9030cf79f21bc7b96d5dd1255646d4c733148fc1
Hassan Waseem <125817271+hassanwaseem06@users.noreply.github.com>
2023-06-08 14:38:33 +0500

    Add files via upload

---
+++ app04.js
@@ -0,0 +1,13 @@
+    alert()
+// // ------------------------Q 01------------------------
+    var var1, var2, var3;
+// // ------------------------Q 02------------------------
+   //    LEGAL VARIABLE NAMES
+    var var1, var_1, _var1, VAR1, n$12;
+// ------------------------Q 03------------------------
+    document.write("<h2>Rules for naming JS variables</h2><br><br>");
+    document.write("Variable names can only contain numbers, alphabets, $ and _ For example $my_1stVariable.<br>");
+    document.write("Variables must begin with a letter, $ _. For example $name, _name or name <br>");
+    document.write("Variable names are case sensitive <br>");
+    document.write("Variable names should not be JS Keywords <br>")
+  
